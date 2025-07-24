96ae6f0c0019ef143e8f716f16146f960550460d
saitej0909 <ca.saitejshetty@gmail.com>
2025-07-24 17:52:55 +0530

    Initial upload

---
+++ server.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+const rateLimit = require('express-rate-limit');
+const morgan = require('morgan');
+
+const app = express();
+
+// Middleware
+app.use(helmet());
+app.use(cors({ origin: '*' }));
+app.use(express.json());
+app.use(morgan('dev'));
+app.use(rateLimit({ windowMs: 1 * 60 * 1000, max: 100 }));
+
+// Routes
+app.get('/api/ping', (req, res) => res.send('Pong 🏓'));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
